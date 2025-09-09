0973c541cde8ad22c3d18959539ce6f7b62f69f9
Pizok <123370367+Pizok@users.noreply.github.com>
2025-09-09 11:21:50 +0700

    Tambah predikat

--- modul_1-5/TugasKelulusan.java
+++ modul_1-5/TugasKelulusan.java
@@ -23,13 +23,24 @@ public class TugasKelulusan {
             
             //Menentukan nilai akhir.//
             double nilaiAkhir = (nilaiTugas*0.4) + (nilaiUjian*0.6);
+            String predicate;
+
+            if (nilaiAkhir < 70) {
+                predicate = "D";
+            } else if (nilaiAkhir > 70 && nilaiAkhir <= 80) {
+                predicate = "C";
+            } else if (nilaiAkhir > 80 && nilaiAkhir <= 90) {
+                predicate = "B";
+            } else {
+                predicate = "A";
+            }
 
             if (nilaiAkhir < 0 || nilaiAkhir > 100) {
                 System.out.println("Nilai akhir tidak valid.");
             } else if (nilaiAkhir >= 60) {
-                System.out.printf("Nilai %.2f, siswa dinyatakan LULUS", nilaiAkhir);
+                System.out.printf("Nilai %.2f, siswa dinyatakan LULUS dengan predikat \"%s\"", nilaiAkhir, predicate);
             } else {
-                System.out.printf("Nilai %.2f, siswa dinyatakan TIDAK LULUS", nilaiAkhir);
+                System.out.printf("Nilai %.2f, siswa dinyatakan TIDAK LULUS dengan predikat \"%s\"", nilaiAkhir, predicate);
             }
         }
     }
