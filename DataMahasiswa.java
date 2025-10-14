06843b12e481f96d1e4109c87cbf02f02bf24fb9
Pizok <123370367+Pizok@users.noreply.github.com>
2025-10-14 09:44:22 +0700

    UTS fix

--- DataMahasiswa.java
+++ DataMahasiswa.java
@@ -5,43 +5,49 @@ class Mahasiswa {
     private String nama;
     private int age;
 
+    // Construcktor Object
     public Mahasiswa(String namaMahasiswa, int umur){
         this.nama = namaMahasiswa;
-        this.age = umur;
+        setUmur(umur);
     }
     
+    // Getter atribut nama
     public String getNama(){
         return nama;
     }
 
+    // Getter atribut umur (age)
     public int getUmur(){
         return age;
     }
 
+    // Setter atribut nama
     public void setNama(String namaMahasiswa){
         this.nama = namaMahasiswa;
     }
 
+    // Setter atribut umur (age)
     public void setUmur(int umur){
-            this.age = umur;
+        this.age = umur;
     }
 
+    // Method untuk menentukan kategori mahasiswa
     public String kategoriUmur(){
-        String hasil;
         if (this.age >= 18) { 
-            hasil = "Mahasiswa Dewasa";
+            return "Mahasiswa Dewasa";
         } else {
-            hasil = "Mahasiswa di bawah umur";
+            return "Mahasiswa di bawah umur";
         }
-        return hasil;
     }
 
+    // Method untuk menghitung tahun lahir mahasiswa
     public int tahunLahir(){
         int tahun = LocalDate.now().getYear();
         int lahir = tahun - this.age;
         return lahir;
     }
 
+    // Method untuk menampilkan data
     public void tampilkanData(){
         System.out.println("Nama: " + this.nama);
         System.out.println("Umur: " + this.age);
@@ -52,24 +58,30 @@ class Mahasiswa {
 
 
 public class DataMahasiswa {
+    // Method untuk input yang tervalidasi
     public static int input(Scanner scan, String perintah, int min, int max) {
         int value = 0;  
         boolean valid = false;
 
         while (valid == false) {
-            System.out.print(perintah);
-            value = scan.nextInt(); 
+            try {
+                System.out.print(perintah);
+                value = scan.nextInt(); 
 
-            if (value >= min && value <= max) {
-                valid = true; 
-            } else {
-                System.out.println("Input harus diantara" + min + " dan " + max + "!");
+                if (value >= min && value <= max) {
+                    valid = true; 
+                } else {
+                    System.out.println("Input harus diantara " + min + " dan " + max + "!");
+                }
+            } catch (Exception e) {
+                System.out.println("Input harus merupakan angka!");
             }
+            scan.nextLine();
         }
-
         return value;
     }
 
+    // Main method
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -77,7 +89,6 @@ public class DataMahasiswa {
 
         // Isi data mahasiswa
         int jumlah = input(scan, "Jumlah mahasiswa: ", 1, 100);
-        scan.nextLine();
 
         Mahasiswa[] mahasiswa = new Mahasiswa[jumlah];
 
@@ -88,12 +99,13 @@ public class DataMahasiswa {
             String nama = scan.nextLine();
             
             int umur = input(scan, "Umur: ", 1, 150);
-            scan.nextLine();
 
             mahasiswa[i] = new Mahasiswa(nama, umur);
         }
         
-        System.out.println("\n--Data Mahasiswa--");
+        // Menampilkan data mahasiswa yang sudah di-input
+        System.out.println("\nData Mahasiswa");
+        System.out.println("---------------------------------");
 
         for (int i = 0; i < mahasiswa.length; i++) {
             System.out.println("\nMahasiswa ke-"+ (i+1));
